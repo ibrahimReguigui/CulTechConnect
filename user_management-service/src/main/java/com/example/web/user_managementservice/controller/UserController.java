4c4e4e5a6811c95b311fe73394a279942f5aefd4
ibrahimReguigui <ibrahim.reguigui@esprit.tn>
2023-10-03 11:15:59 +0100

    registration entity validation added

--- user_management-service/src/main/java/com/example/web/user_managementservice/controller/UserController.java
+++ user_management-service/src/main/java/com/example/web/user_managementservice/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.web.user_managementservice.dto.UserDto;
 import com.example.web.user_managementservice.service.UserService;
 import com.example.web.user_managementservice.service.UserServiceImp;
 import jakarta.annotation.security.RolesAllowed;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.KeycloakPrincipal;
@@ -16,12 +17,16 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @CrossOrigin(origins = "*")
@@ -51,8 +56,16 @@ public class UserController {
     }
 
     @PostMapping("/visitor/register")
-    public ResponseEntity registration(@RequestBody UserDto userDto) {
-        System.out.println("register");
+    public ResponseEntity registration(@RequestBody @Valid UserDto userDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> validationErrors = bindingResult.getAllErrors()
+                    .stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .collect(Collectors.toList());
+            return ResponseEntity
+                    .badRequest()
+                    .body(validationErrors);
+        }
         String result = userService.registration(userDto);
         if (result == "User Already Exist")
             return ResponseEntity.unprocessableEntity().body(Map.of("response", result));
